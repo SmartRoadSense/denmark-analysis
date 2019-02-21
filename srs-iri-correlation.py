90d0ed802e7d630f58ee8a4510f1f2dbedc1a07c
Saverio Delpriori <saverio.delpriori@uniurb.it>
2019-02-21 14:25:05 +0100

    Add ETA stats

--- srs-iri-correlation.py
+++ srs-iri-correlation.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import geopy.distance
+import time, datetime
 
 AGGREGATION_RADIUS = 10
 
@@ -25,12 +26,12 @@ def get_PPE_aggregation(lat, long, data):
         distance = get_geographical_distance(lat, long, point[2], point[1])
 
         if distance < AGGREGATION_RADIUS:
-            print("distance {0}".format(distance))
+            #print("distance {0}".format(distance))
             ppe.append(point[0])
 
     ppe_avg = np.average(ppe)
 
-    return ppe_avg, distance
+    return ppe_avg
 
 
 ppe_data = pd.read_csv("data/denmark-srs-data.csv", delimiter=";")
@@ -39,24 +40,35 @@ iri_data = pd.read_csv("data/denmark-iri-data.csv", delimiter=";")
 print(ppe_data.head())
 
 iri_data = iri_data.filter(items=['Distance [m]', 'Latitude', 'Longitude', 'IRI(1)', 'IRI(17)'])
+iri_data['AVG IRI'] = iri_data.apply(lambda row: (row['IRI(1)'] + row['IRI(17)'])/2, axis=1)
+
+#iri_data_1 = iri_data.iloc[:round(len(iri_data)/2), :]
+#iri_data_2 = iri_data.iloc[round(len(iri_data)/2):, :]
+
 print(iri_data.head())
 
 output = pd.DataFrame(columns=['distance', 'latitude', 'longitude', 'iri', 'ppe'])
+start = time.time()
+
 
 for index, row in iri_data.iterrows():
     print("{0}/{1}".format(index, len(iri_data)))
-    iri = get_IRI(row)
+
     ppe = get_PPE_aggregation(row[1], row[2], ppe_data)
 
-    output = output.append({'distance': row[0], 'latitude': row[1], 'longitude': row[2], 'iri': iri, 'ppe': ppe},
+    output = output.append({'distance': row[0], 'latitude': row[1], 'longitude': row[2], 'iri': row[5], 'ppe': ppe},
                            ignore_index=True)
-    if index > 3:
-        break
+
+    end = time.time()
+    elapsed_time = end - start
+    #print("elapsed time {0} ".format(elapsed_time))
+
+    iteration = index + 1
+    seconds = (elapsed_time) * (len(iri_data) / iteration)
+    completion_percentage = (iteration * 100)/len(iri_data)
+    eta = str(datetime.timedelta(seconds=(seconds-elapsed_time)))
+    print("Time for completion {0} - ETA: {1} - Completion {2:.2f}%".format(str(datetime.timedelta(seconds=seconds)), eta, completion_percentage))
 
 
 output.to_csv('data/srs-iri-data.csv', sep=';', encoding='utf-8')
-
-print(output['iri'])
-print(output['ppe'])
-
 print(np.corrcoef(output['iri'], output['ppe']))
